564eeae4c091c7403c7755f437d989e385564296
choi-jong-hyun1028 <81130130+choi-jong-hyun1028@users.noreply.github.com>
2021-11-04 15:52:24 +0900

    UPload_file
    파일이 많아서 전부 올리지 못했습니다. 주요 코드만 올려요.

---
+++ Image-processing_code/final.py
@@ -0,0 +1,32 @@
+from KNeTest import getresult
+from selenium import webdriver
+import time
+import urllib.request
+
+web = webdriver.Chrome()
+web.get('http://192.168.0.20:10000')
+
+# time.sleep(0.5)
+time.sleep(2)
+web.find_element_by_xpath('/html/body/div[1]/div/div/blockquote/a').click()
+
+for i in range(1,31):
+    time.sleep(0.2)
+    img = web.find_element_by_id('img'+str(i))
+    src = img.get_attribute('src')
+    # download the image
+    urllib.request.urlretrieve(src, './examimg/img'+str(i)+'.png')
+    time.sleep(0.2)
+    result = getresult('./examimg/img'+str(i)+'.png')
+    print(result)
+    web.find_element_by_id('ans').send_keys(result)
+    web.find_element_by_id('submit').click()
+
+time.sleep(5)
+web.quit()
+
+# result = getresult('beforeimages/1.png')
+# print(result)
+#
+# result = getresult('beforeimages/2.png')
+# print(result)
